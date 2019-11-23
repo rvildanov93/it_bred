6f30d3d3003dc98730ce62441bdca712ec5c6fbb
Ramil Vildanov <rrizakuli@gmail.com>
2019-11-23 07:07:31 +0300

    Update gtag.js

--- js/gtag.js
+++ js/gtag.js
@@ -1,7 +1,9 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-152954055-1"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-153122300-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'UA-152954055-1');
+
+  gtag('config', 'UA-153122300-1');
 </script>
